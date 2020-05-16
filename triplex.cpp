20baef203ef6bf9573593b3a77abf4db15e7725f
Amaan Shawkath <amaanshaw@live.co.uk>
2020-05-16 02:12:50 +0100

    better commenting/variable names

--- triplex.cpp
+++ triplex.cpp
@@ -2,7 +2,24 @@
 
 int main()
 {
-    std::cout << "The Wendigo has been unleashed into this world! Your Mission: return the beast from whence it came!";
-    std::cout << "You need to decipher the ancient locks in order to retrieve the artefacts needed to return the beast";
+    // prints the following text to the console (welcome message)
+    std::cout << "The Wendigo has been unleashed into this world! Your Mission: return the beast from whence it came!" << std::endl;
+    std::cout << "You need to decipher the ancient locks in order to retrieve the artefacts needed to return the beast" << std::endl;
+
+    const int CodeA = 4;
+    const int CodeB = 10;
+    const int CodeC = 50;
+
+    int CodeSum = CodeA + CodeB + CodeC;
+    int CodeProduct = CodeA * CodeB * CodeC;
+
+    // print CodeSum and CodeProduct to the console
+    std::cout << std::endl;
+    std::cout << "+ There are 3 numbers in the code" << std::endl;
+    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
+    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
+
+    int PlayerGuess;
+
     return 0;
 }
