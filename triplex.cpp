bba1fc05629eaf8641b8f9fc6a13bad71d80c1b5
Amaan Shawkath <amaanshaw@live.co.uk>
2020-05-16 01:15:47 +0100

    inital welcome messages

--- triplex.cpp
+++ triplex.cpp
@@ -1,4 +1,8 @@
+#include <iostream>
+
 int main()
 {
+    std::cout << "The Wendigo has been unleashed into this world! Your Mission: return the beast from whence it came!";
+    std::cout << "You need to decipher the ancient locks in order to retrieve the artefacts needed to return the beast";
     return 0;
 }
