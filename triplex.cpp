b1e837344888492ca162b7e08ae0aefb661b2042
Amaan Shawkath <amaanshaw@live.co.uk>
2020-05-16 10:18:24 +0100

    moving code into functions adding some ASCII art

--- triplex.cpp
+++ triplex.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 
-int main()
+void PrintIntroduction()
 {
     // prints the following text to the console (welcome message)
+    std::cout << R"(     ___,---.__          /'|`\          __,---,___
+    ,-'    \`    `-.____,-'  |  `-.____,-'    //    `-.
+  ,'        |           ~'\     /`~           |        `.
+ /      ___//              `. ,'          ,  , \___      \
+|    ,-'   `-.__   _         |        ,    __,-'   `-.    |
+|   /          /\_  `   .    |    ,      _/\          \   |
+\  |           \ \`-.___ \   |   / ___,-'/ /           |  /
+ \  \           | `._   `\\  |  //'   _,' |           /  /
+  `-.\         /'  _ `---'' , . ``---' _  `\         /,-'
+     ``       /     \    ,='/ \`=.    /     \       ''
+             |__   /|\_,--.,-.--,--._/|\   __|
+             /  `./  \\`\ |  |  | /,//' \,'  \
+            /   /     ||--+--|--+-/-|     \   \
+           |   |     /'\_\_\ | /_/_/`\     |   |
+            \   \__, \_     `~'     _/ .__/   /
+             `-._,-'   `-._______,-'   `-._,-'
+   )" << '\n';
+
     std::cout << "The Wendigo has been unleashed into this world! Your Mission: return the beast from whence it came!\n";
     std::cout << "You need to decipher the ancient locks in order to retrieve the artefacts needed to return the beast\n\n";
+}
+
+void PlayGame()
+{
+    PrintIntroduction();
 
     const int CodeA = 2;
     const int CodeB = 3;
@@ -17,7 +40,7 @@ int main()
     std::cout << std::endl;
     std::cout << "+ There are 3 numbers in the code";
     std::cout << "\n+ The codes add-up to: " << CodeSum;
-    std::cout << "\n+ The codes multiply to give: ";
+    std::cout << "\n+ The codes multiply to give: " << CodeProduct << std::endl;
 
     // Store player guess
     int GuessA, GuessB, GuessC;    
@@ -35,6 +58,10 @@ int main()
     {
         std::cout << "\nYou Lose!";
     }
+}
 
+int main()
+{
+    PlayGame();
     return 0;
 }
